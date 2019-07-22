97fd0a4cd38f2cad527b77c63d848145e583a9f2
jacke <p15583101939@gmail.com>
2019-07-22 16:31:07 +0800

    新增工具类 TextUtil support库版本升级到0.0.6 androidx库版本升级到0.0.7 androidx库的material依赖库版（support库中叫design库）本升级到最新版

---
+++ x/src/main/java/com/sscl/baselibrary/utils/TextUtil.java
@@ -0,0 +1,21 @@
+package com.sscl.baselibrary.utils;
+
+import android.text.InputType;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+
+public class TextUtil {
+
+    public static void showEditTextPassword(@NonNull EditText passwordEt, boolean showPassword){
+        if (showPassword) {
+            passwordEt.setInputType(InputType.TYPE_CLASS_TEXT);
+            //setTransformationMethod 支持将输入的字符转换，包括清除换行符、转换为掩码
+            passwordEt.setTransformationMethod(android.text.method.HideReturnsTransformationMethod.getInstance());
+        } else {
+            passwordEt.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            passwordEt.setTransformationMethod(android.text.method.PasswordTransformationMethod.getInstance());
+        }
+        passwordEt.setSelection(passwordEt.getText().toString().length());
+    }
+}
